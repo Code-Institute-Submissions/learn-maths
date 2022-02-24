1ece1d5d3f87b96dfd915067628ec194c9aa1e33
faridjos <farid.benachenhou@hotmail.com>
2022-02-24 10:49:35 +0000

    comment Javascript file script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
-//Practice
+//Practice area
 
+//Add event listener to user number input field in practice area
 document.getElementById('user-number').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         writeNumbers();
     }
 })
 
+//Display sum squared, difference squared and conjugate product questions
 function writeNumbers() {
+
+    //Empty input fields
     zeroeFields();
     let userNumber = parseInt(document.getElementById('user-number').value);
+
+    //Warnings for invalid user numbers
     if (Number.isNaN(userNumber) || (userNumber <= 5)) {
         alert("The number must be greater than 5!");
         document.getElementById('user-number').value = '';
@@ -16,6 +22,8 @@ function writeNumbers() {
         alert("The number must not be a multiple of 10!");
         document.getElementById('user-number').value = '';
     } else {
+
+        //Display numbers in the three questions
         let mod = userNumber % 10;
         let numberSum = document.getElementsByClassName('number-sum');
         let numberDiff = document.getElementsByClassName('number-diff');
@@ -37,8 +45,10 @@ function writeNumbers() {
     }
 }
 
+//Add event listener to the three check answers buttons in practice area
 for (let i = 0; i <= 2; i++) {
     document.getElementsByClassName('check-answers')[i].addEventListener('click', function () {
+        //Color buttons according to answer
         if (checkAnswers[i]()) {
             document.getElementsByClassName('check-answers')[i].setAttribute("style", "background-color: rgb(3, 250, 24)");
         } else {
@@ -48,9 +58,12 @@ for (let i = 0; i <= 2; i++) {
 }
 
 let checkAnswers = [];
-
+//The three functions check the answer in all steps of the three calculation
+//as well as in the final result
 for (let i = 0; i <= 2; i++) {
     checkAnswers.push(function () {
+
+        //Check the tens squared step in all three calculations
         let numberSum = parseInt(document.getElementsByClassName('number-sum')[0].textContent);
         let ones = numberSum % 10;
         let tens = numberSum - ones;
@@ -61,6 +74,7 @@ for (let i = 0; i <= 2; i++) {
             return correct;
         }
 
+        //Check the double product step in sum squared and difference squared calculation
         if (i != 1) {
             let j;
             if (i == 0) {
@@ -75,12 +89,14 @@ for (let i = 0; i <= 2; i++) {
             }
         }
 
+        //Check the ones squared step in all three calculations
         let squareOne = document.getElementsByClassName('square-ones')[i];
         if (squareOne.value != ones ** 2) {
             correct = false;
             return correct;
         }
 
+        //Checks the final result in all three calculations
         if (i === 0) {
             let resultSum = parseInt(document.getElementById('result-sum').value);
             if (resultSum !== tens ** 2 + 2 * tens * ones + ones ** 2) {
@@ -104,6 +120,7 @@ for (let i = 0; i <= 2; i++) {
     })
 }
 
+//Empty all input fields in practice area
 function zeroeFields() {
 
     let numberSum = document.getElementsByClassName('number-sum');
@@ -135,6 +152,7 @@ function zeroeFields() {
         result.value = '';
     }
 
+    //Remove coloring of buttons
     let checkAnswers = document.getElementsByClassName('check-answers');
     for (let checkAnswer of checkAnswers) {
         checkAnswer.removeAttribute("style");
@@ -142,28 +160,39 @@ function zeroeFields() {
 }
 
 //Challenge
+
+//Add event listener to Start Game button in challenge area
 let started = false;
 document.getElementById('start-game').addEventListener('click', function () {
     if (!started) {
         started = true;
+
+        //Add event listener to answer input field
         document.getElementById("answer-box").addEventListener('keydown', function (event) {
             if (event.key === 'Enter') {
                 checkAnswer();
             }
         })
         writeQuestion();
+
+    //Warning if user starts the game again without answering
     } else {
         alert("You already started the challenge! Answer the question!")
     }
 });
 
+//Generates random number and random type of question
+//(either a square or a conjugate product)
 function generateQuestion() {
     let num1;
     let num2;
     do {
+        //Generate random number
         let tens = Math.floor(Math.random() * 10) + 1;
         let ones = Math.floor(Math.random() * 9) + 1;
         num1 = tens * 10 + ones;
+
+        //Generate random type of question (square or conjugate product)
         let calculateSquare = Math.floor(Math.random() * 2);
         if (calculateSquare) {
             num2 = num1;
@@ -178,13 +207,18 @@ function generateQuestion() {
     return [num1, num2];
 }
 
+//Display question in challenge area
 function writeQuestion() {
     let num = generateQuestion();
     document.getElementById('first-number').innerText = num[0];
+
+    //Square
     if (num[0] === num[1]) {
         document.getElementById('exponent').innerText = 2;
         document.getElementById('multiply-sign').innerText = '';
         document.getElementById('second-number').innerText = '';
+
+    //Conjugate product
     } else {
         document.getElementById('exponent').innerText = '';
         document.getElementById('multiply-sign').innerText = ' x ';
@@ -192,51 +226,73 @@ function writeQuestion() {
     }
 }
 
+// Check answer in challenge area
 function checkAnswer() {
     let firstNumber = parseInt(document.getElementById('first-number').textContent);
     let secondNumber = parseInt(document.getElementById('second-number').textContent);
     let userAnswer = parseInt(document.getElementById('answer-box').value);
+
+    //Increment number of trials
     incrementTotalAnswers();
+
+    //Type of question is square
     if (Number.isNaN(secondNumber)) {
         let rightAnswer = firstNumber ** 2;
+       
         if (rightAnswer != userAnswer) {
+
+            //Answer box flashes red for 0.5 s
             document.getElementById('answer-box').setAttribute("style", "background-color: red");
             setTimeout(function () {
                 document.getElementById('answer-box').removeAttribute("style");
             }, 500);
         } else {
+
+            //Answer box flashes green for 0.5 s
             document.getElementById('answer-box').setAttribute("style", "background-color: rgb(3, 250, 24)");
             setTimeout(function () {
                 document.getElementById('answer-box').removeAttribute("style");
             }, 500);
+
+            //Increment score
             incrementCorrectAnswers();
         }
+    //Type of question is conjugate product
     } else {
         let rightAnswer = firstNumber * secondNumber;
         if (rightAnswer != userAnswer) {
+
+            //Answer box flashes red for 0.5 s
             document.getElementById('answer-box').setAttribute("style", "background-color: red");
-            setTimeout(function () {
+            setTimeout(function () {               
                 document.getElementById('answer-box').removeAttribute("style");
             }, 500);
         } else {
+
+            //Answer box flashes green for 0.5 s
             document.getElementById('answer-box').setAttribute("style", "background-color: rgb(3, 250, 24)");
+            
+            //Increment score
             incrementCorrectAnswers();
             setTimeout(function () {
                 document.getElementById('answer-box').removeAttribute("style");
             }, 500);
         }
     }
+    //Empty answer input field
     document.getElementById('answer-box').value = '';
     document.getElementById('answer-box').focus();
     writeQuestion();
 }
 
+//Increment score
 function incrementCorrectAnswers() {
     let correct = parseInt(document.getElementById('correct').innerText);
     document.getElementById('correct').innerText = ++correct;
 
 }
 
+//Increment number of trials
 function incrementTotalAnswers() {
     let total = parseInt(document.getElementById('total').innerText);
     document.getElementById('total').innerText = ++total;
